329e34068e9baf0f4a1115ca991392ceac021b9d
sasha-lobov <xeroalex2015@gmail.com>
2024-01-10 18:06:07 +0300

    commit_alex_21112023

--- main_script.py
+++ main_script.py
@@ -23,39 +23,35 @@ if __name__ == "__main__":
     while True:
 
         _, img = cap.read()
-
-        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        print(img.shape)
+        gray = cv2.cvtColor(img[:][:600], cv2.COLOR_RGB2GRAY)
         blur = cv2.GaussianBlur(gray, (5, 5), 100)
 
-        alpha = 1.8  # Contrast control (1.0-3.0)
+        alpha = 1.5  # Contrast control (1.0-3.0)
         beta = 0  # Brightness control (0-100)
 
         contrast = cv2.convertScaleAbs(blur, alpha=alpha, beta=beta)
 
         canny = cv2.Canny(contrast, 230, 255)
+        kernel = np.ones((7, 7), np.uint8)
+        erode = cv2.erode(canny, kernel, iterations=1)
 
-        kernel = np.ones((5, 5), np.uint8)
-        dil = cv2.dilate(canny, kernel, iterations=3)
-        er = cv2.erode(dil, kernel, iterations=3)
-
-        matrix = np.copy(er).astype('uint8')
-
-        for k in piano.centers:
-            area_1 = matrix[100][k] > 0 or matrix[99][k] > 0 or matrix[98][k] > 0
-            area_2 = matrix[97][k] > 0 or matrix[96][k] > 0 or matrix[95][k] > 0
-            area_3 = matrix[94][k] > 0 or matrix[93][k] > 0 or matrix[92][k] > 0
+        for i in range(0, len(piano.centers)):
+            area_1 = canny[100][piano.centers[i]] > 0 or canny[99][piano.centers[i]] > 0 or canny[98][piano.centers[i]] > 0
+            area_2 = canny[97][piano.centers[i]] > 0 or canny[96][piano.centers[i]] > 0 or canny[95][piano.centers[i]] > 0
+            area_3 = canny[94][piano.centers[i]] > 0 or canny[93][piano.centers[i]] > 0 or canny[92][piano.centers[i]] > 0
             if area_1 or area_2 or area_3:
-                cv2.circle(matrix, (k, 100), 20, (255, 0, 0), -1)
+                cv2.circle(img, (piano.centers[i], 100), 20, (255, 0, 0), -1)
 
-        for k in piano.centers:
-            cv2.circle(matrix, (k, 100), 1, (255, 0, 0), -1)
+        for i in range(0, len(piano.centers)):
+            cv2.circle(img, (piano.centers[i], 100), 1, (255, 0, 0), -1)
 
         # Рисуем центры всех клавиш
         for i in range(0, len(piano.centers)):
             for p in range(0, 1000):
-                cv2.circle(matrix, (piano.centers[i], img.shape[0]-90-p), 1, (255, 255, 255), -1)
+                cv2.circle(img, (piano.centers[i], img.shape[0]-90-p), 1, (255, 255, 255), -1)
 
-        cv2.imshow('res', matrix)
+        cv2.imshow('res', img)
 
         if cv2.waitKey(50) & 0xFF == ord('q'):
             break
